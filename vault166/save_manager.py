61f0bdbfba22f3d0e344f3b33eaae89e5e2d60dd
magdielrosario97 <msrdev.tech@gmail.com>
2026-05-02 18:50:32 -0500

    feat: add save method to SaveManager class

--- vault166/save_manager.py
+++ vault166/save_manager.py
@@ -5,3 +5,8 @@ class SaveManager:
     def __init__(self):
         self.conn = get_db_connection()
         initialize_db(self.conn)
+
+    def save(self, slot_name, player, rooms) -> str:
+        save_game(self.conn, slot_name, player, rooms)
+        message = f"Game saved to slot '{slot_name}'."
+        return message
