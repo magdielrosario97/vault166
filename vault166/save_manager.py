11f237ecce68280a85e43fde9b28f316830f459f
magdielrosario97 <msrdev.tech@gmail.com>
2026-05-02 18:51:12 -0500

    feat: add load method to SaveManager class

--- vault166/save_manager.py
+++ vault166/save_manager.py
@@ -10,3 +10,12 @@ class SaveManager:
         save_game(self.conn, slot_name, player, rooms)
         message = f"Game saved to slot '{slot_name}'."
         return message
+
+    def load(self, slot_name, player, rooms) -> tuple[bool, str]:
+        success = load_game(self.conn, slot_name, player, rooms)
+        if success:
+            message = f"Game loaded from slot '{slot_name}'."
+        else:
+            message = f"Save slot '{slot_name}' not found."
+
+        return success, message
